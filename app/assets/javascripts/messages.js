f1aafab14d92f14f476922110077fc945fefd73c
sasakurenomai <masaoka53644@gmail.com>
2020-03-17 17:22:36 +0900

    chat-space-automatic-update

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,11 +1,11 @@
 $(function(){
-  function buildHTML(message){
-    if ( message.image ) {
-      var html =
-       `<div class='chat-main__messages__message'>
-          <div class='chat-main__messages__message__upper-info'>
+
+  var buildHTML = function(message){
+    if ( message.body && message.image ) {
+      var html = `<div class='chat-main__messages__message' data-message-id=` + message.id + `>` +
+          `<div class='chat-main__messages__message__upper-info'>
             <div class='chat-main__messages__message__upper-info__talker'>
-              ${message.user_name}
+                ${message.user_name}
             </div>
             <div class='chat-main__messages__message__upper-info__date'>
               ${message.created_at}
@@ -16,14 +16,11 @@ $(function(){
               ${message.body}
             </p>
           </div>
-          </div>
           <img src=${message.image} ></img>
         </div>`
-      return html;
-    } else {
-      var html =
-        `<div class='chat-main__messages__message'>
-          <div class='chat-main__messages__message__upper-info'>
+    } else if (message.body){
+      var html = `<div class='chat-main__messages__message' data-message-id=` + message.id + `>` +
+          `<div class='chat-main__messages__message__upper-info'>
             <div class='chat-main__messages__message__upper-info__talker'>
               ${message.user_name}
             </div>
@@ -39,9 +36,21 @@ $(function(){
             <div class='chat-main__messages__message__upper-info__delete'>
             </div>
         </div>`
-      return html;
+    } else if (message.image){
+      var html = `<div class='chat-main__messages__message' data-message-id=` + message.id + `>` +
+      `<div class='chat-main__messages__message__upper-info'>
+        <div class='chat-main__messages__message__upper-info__talker'>
+          ${message.user_name}
+        </div>
+        <div class='chat-main__messages__message__upper-info__date'>
+          ${message.created_at}
+        </div>
+      </div>
+      <img src=${message.image} ></img>
+    </div>`
     };
-  }
+    return html;
+  };
   $('#new_message').on('submit', function(e){
     e.preventDefault()
     var formData = new FormData(this);
@@ -65,6 +74,41 @@ $(function(){
       alert("メッセージ送信失敗・・・・・");
     })
   });
+
+  
+  var reloadMessages = function() {
+    
+    var last_message_id = $('.chat-main__messages__message:last').data("message-id");
+    console.log(last_message_id)
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json', 
+      data: {id: last_message_id}
+    })
+  
+    .done(function(messages) {
+      if (messages.length !== 0) {
+      var insertHTML = '';
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      $('.messages').append(insertHTML);
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      }
+
+    })
+
+
+    .fail(function() {
+      alert('error');
+    });
+  };
+
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  } 
+
 });
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,9 @@ Rails.application.routes.draw do
   resources :messages, only: [:destroy]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create, :edit, :update]
+    namespace :api do
+      resources :messages, only: :index, defaults: { format: 'json' }
+    end
   end
+
 end
